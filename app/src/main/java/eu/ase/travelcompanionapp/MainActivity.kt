1e11936a3884f5c3086f120b01ef5126092ac9e2
AndreiHammer <andreiciocan04@gmail.com>
2024-11-29 19:13:29 +0200

    Initial commit

--- app/src/main/java/eu/ase/travelcompanionapp/MainActivity.kt
+++ app/src/main/java/eu/ase/travelcompanionapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TravelCompanionAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hammer",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TravelCompanionAppTheme {
-        Greeting("Android")
+        Greeting("Hammer")
     }
 }
