50b98b0eb2830a5512769920ee6cd1fa74b63a88
AndreiHammer <andreiciocan04@gmail.com>
2025-04-27 16:49:46 +0300

    improved booking history screen

--- app/src/main/java/eu/ase/travelcompanionapp/booking/presentation/bookinghistory/components/BookingTabContent.kt
+++ app/src/main/java/eu/ase/travelcompanionapp/booking/presentation/bookinghistory/components/BookingTabContent.kt
@@ -14,10 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import eu.ase.travelcompanionapp.R
 import eu.ase.travelcompanionapp.booking.domain.models.BookingInfo
 import eu.ase.travelcompanionapp.hotel.domain.model.Hotel
 
